9b4e6b4dfcbcfc315b85c18ac5843252e6740dc4
DogManRunOut <105308346+DogManRunOut@users.noreply.github.com>
2025-01-04 05:43:00 +0500

    Add files via upload

---
+++ roblox_exp.py
@@ -0,0 +1,22 @@
+def __credits__(self):
+	self.__author__ = "Wendigo"
+	self.__putch__ = "exp-count machine"
+	self.__game__ = "Roblox/Muscle Legends"
+
+count = 0
+calc = 0
+for i in range(n):
+	count += c
+	calc += count
+	# print(count)
+print(calc)
+
+'''
+# print(count) : optional, string
+let see how much exp need pet or aura for this level (down-side, from 1 to 20)
+usable if you deleted "#" before string
+
+n, c : bool, int
+n — exp for level you need
+c — exp of pet or aura on 1 level (for example, if unique pet need 1250 exp, use this number to count)
+'''
